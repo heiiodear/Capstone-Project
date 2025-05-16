b5c4425b1416525f786be15360ca16e4428f3fd2
Yanisa Klongkleaw <julia12a2005@gmail.com>
2025-05-16 23:46:23 +0700

    RTSP and yml

--- ai/testRTSP/RTSP.py
+++ ai/testRTSP/RTSP.py
@@ -2,7 +2,7 @@ import time
 import cv2
 import subprocess
 
-camera_index = 1
+camera_index = 0
 cap = cv2.VideoCapture(camera_index)
 
 if not cap.isOpened():
@@ -13,29 +13,43 @@ width = 1280
 height = 720
 fps = 30
 
+# ffmpeg_command = [
+#     'ffmpeg',
+#     '-f', 'rawvideo',
+#     '-pix_fmt', 'bgr24',
+#     '-s', f'{width}x{height}',
+#     '-r', str(fps),
+#     '-i', '-',
+#     '-c:v', 'libx264',
+#     '-preset', 'veryfast',
+#     '-tune', 'zerolatency', 
+#     '-profile:v', 'high',   
+#     '-g', '25',
+#     '-keyint_min', '25',
+#     '-b:v', '1500k',
+#     '-bufsize', '1500k',
+#     '-bf', '0',
+#     '-threads', '1',
+#     '-vsync', '1',
+#     '-x264opts', 'keyint=25',  
+#     '-bsf:v', 'h264_mp4toannexb',  
+#     '-f', 'rtsp',
+#     '-rtsp_transport', 'tcp',
+#     'rtsp://localhost:8554/live'
+# ]
+
 ffmpeg_command = [
-    'ffmpeg',
-    '-f', 'rawvideo',
-    '-pix_fmt', 'bgr24',
-    '-s', f'{width}x{height}',
-    '-r', str(fps),
-    '-i', '-',
-    '-c:v', 'h264_qsv',
-    '-preset', 'veryfast',
-    '-tune', 'zerolatency', 
-    '-profile:v', 'high',   
-    '-g', '25',
-    '-keyint_min', '25',
-    '-b:v', '1500k',
-    '-bufsize', '1500k',
-    '-bf', '0',
-    '-threads', '1',
-    '-vsync', '1',
-    '-x264opts', 'keyint=25',  
-    '-bsf:v', 'h264_mp4toannexb',  
-    '-f', 'rtsp',
-    '-rtsp_transport', 'tcp',
-    'rtsp://localhost:8554/live'
+    "ffmpeg",
+    "-f", "rawvideo",
+    "-pix_fmt", "bgr24",
+    "-s", "1280x720",
+    "-r", "30",
+    "-i", "-",
+    "-vf", "format=yuv420p",         # ✅ force supported pixel format
+    "-c:v", "libx264",
+    "-preset", "ultrafast",
+    "-f", "rtsp",
+    "rtsp://localhost:8554/mystream"
 ]
 
 process = subprocess.Popen(ffmpeg_command, stdin=subprocess.PIPE)
@@ -59,7 +73,7 @@ try:
             print("ไม่สามารถส่งข้อมูลไปยัง FFmpeg ได้ (Broken Pipe)")
             break
 
-        time.sleep(0.3)
+        time.sleep(0.2)
 
 finally:
     cap.release()
