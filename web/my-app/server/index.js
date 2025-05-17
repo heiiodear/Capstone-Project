cf254e336c6b6bbbf86679fa23170a0111570565
heiiodear <athitanm2@gmail.com>
2025-05-17 16:17:53 +0700

    update token expires

--- web/my-app/server/index.js
+++ web/my-app/server/index.js
@@ -68,7 +68,9 @@ app.post("/login", async (req, res) => {
         return res.status(400).json({ error: "Incorrect password" });
       }
   
-      const token = jwt.sign({ id: user._id, email: user.email }, "your-secret-key");
+      const token = jwt.sign({ id: user._id, email: user.email }, "your-secret-key", {
+        expiresIn: "30d",
+      });
   
       res.status(200).json({ message: "Login successful", token, user: { _id: user._id } });
   
